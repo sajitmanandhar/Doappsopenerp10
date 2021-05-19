0889ed849117f3bf6c085f5188f74640477cf2e4
NyayaHealthNepal <sajit.manandhar@nyayahealthnepal.org>
2021-05-19 19:16:58 +0545

    Initial Commit

---
+++ __manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name' : 'DOapp Application',
+    'description': 'Manage your Personal Do-app tasks.',
+    'author': 'Daniel Reis',
+    'depends': ['base'],
+    'application': True,
+
+    'data': [
+             'security/todo_access_rules.xml',
+             'views/todo_menu.xml',
+             'views/todo_view.xml',
+    
+    ],
+    
+    
+    
+
+}
+
---
+++ todo_model.py
@@ -0,0 +1,20 @@
+# _*_ coding: utf-8 _*_
+
+from odoo import models, fields,api
+class TodoTask(models.Model):
+	#@api.multi
+	#def do_toggle_done(self):
+		#for task in self:
+			#task.is_done = not task.is_done
+		#return True	-->
+
+	@api.model
+	def do_clear_done(self):
+		dones = self.search([('is_done', '=', True)])
+		dones.write({'active' : False})
+		return True		
+	_name= 'todo.task'
+	_description = 'DOapp Task'
+	name = fields.Char('Description', required=True)
+	is_done = fields.Boolean('Done?')
+	active = fields.Boolean('Active?', default=True)
